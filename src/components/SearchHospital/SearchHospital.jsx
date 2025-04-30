1eca7745de740cbc821e7627866c811c0fcc1739
Kamlesh-Sahu-R <kamleshsahur@gmail.com>
2025-04-30 14:25:04 +0530

    made changes

--- src/components/SearchHospital/SearchHospital.jsx
+++ src/components/SearchHospital/SearchHospital.jsx
@@ -116,6 +116,7 @@ export default function SearchHospital() {
                 ))}
             </Select>
             <Button
+                id="searchBtn"
                 type="submit"
                 variant="contained"
                 size="large"
